6f9669168aba0fbcdc09ac279bab03d547747ae9
Mrudula Pedamallu <mrudulasankar2007@gmail.com>
2025-05-09 16:36:27 +0530

    Delete trail.py

--- trail.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_path = "G:\\Sem1\\actualchatbotrasa\\models\\openhermes-2-mistral-7b.Q4_K_M.gguf"
-model = AutoModelForCausalLM.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-print("Model loaded successfully!")
